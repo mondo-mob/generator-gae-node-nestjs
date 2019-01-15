4a8c99d62a732e9b009e925b0a0ca90662cc1960
Matt Byrne <matt@3wks.com.au>
2019-01-15 14:44:02 +1100

    Kill script fixes and improvements ...
     - use 'awk' to get the pid after doing ps, because 'cut' was not working when pid had leading spaces
     - there were some other rogue node processes that we could kill ... do it!

--- generators/app/templates/server/scripts/kill-server.sh
+++ generators/app/templates/server/scripts/kill-server.sh
@@ -8,12 +8,12 @@ if [[ -n "${kill_options}" ]]; then
 fi
 
 echo "Killing npx server processes for this project ..."
-for pid in $(ps | grep "npx server" | grep -v grep | cut -d" " -f1); do
+for pid in $(ps | grep "npx server" | grep -v grep | awk '{print $1;}'); do
     # Double check that each "npx server" is actually for our project
     grep_result=$(lsof -p ${pid} | grep DIR | grep "${server_path_portion}")
     if [[ -n "${grep_result}" ]]; then
         echo "  - killing pid ${pid}"
-        kill "${kill_options}" ${pid}
+        kill ${kill_options} ${pid}
     fi
 done
 echo "...done"
@@ -22,6 +22,13 @@ echo
 echo "Killing processes listening to tcp ports ${server_tcp_ports} ..."
 for pid in $(lsof -i tcp:${server_tcp_ports} -t); do
     echo "  - killing pid ${pid}"
-    kill "${kill_options}" ${pid}
+    kill ${kill_options} ${pid}
+done
+echo "...done"
+
+echo "Killing node processes executing gae-node-nestjs/server-start.js"
+for pid in $(ps | grep node | grep -v grep | grep "gae-node-nestjs/server-start.js" | grep "${server_path_portion}" | awk '{print $1;}'); do
+    echo "  - killing pid ${pid}"
+    kill ${kill_options} ${pid}
 done
 echo "...done"
