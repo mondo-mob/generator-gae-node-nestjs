0bed58f3d7b8ce9fc89eaac784c8a7c0b46a0d45
kamesh <kamesh@3wks.com.au>
2019-02-15 13:20:37 +1100

    Made User.name optional so user list page doesn't fail after inviting an user

--- generators/app/templates/client/src/graphql.ts
+++ generators/app/templates/client/src/graphql.ts
@@ -18,22 +18,6 @@ export interface Me {
 // This file was automatically generated and should not be edited.
 
 // ====================================================
-// GraphQL query operation: AllUsers
-// ====================================================
-
-export interface AllUsers_users {
-  id: string;
-  email: string;
-}
-
-export interface AllUsers {
-  users: AllUsers_users[];
-}
-
-/* tslint:disable */
-// This file was automatically generated and should not be edited.
-
-// ====================================================
 // GraphQL mutation operation: ActivateAccount
 // ====================================================
 
@@ -82,214 +66,11 @@ export interface ResetPasswordVariables {
 // This file was automatically generated and should not be edited.
 
 // ====================================================
-// GraphQL mutation operation: CreatePermit
-// ====================================================
-
-export interface CreatePermit_createPermit {
-  id: string;
-}
-
-export interface CreatePermit {
-  createPermit: CreatePermit_createPermit;
-}
-
-export interface CreatePermitVariables {
-  application: PermitApplication;
-}
-
-/* tslint:disable */
-// This file was automatically generated and should not be edited.
-
-// ====================================================
-// GraphQL query operation: AllPermits
-// ====================================================
-
-export interface AllPermits_permits_items_status {
-  key: string;
-  description: string;
-}
-
-export interface AllPermits_permits_items {
-  id: string;
-  name: string;
-  description: string;
-  dateAndTime: any;
-  location: string;
-  mobileNumber: string | null;
-  email: string;
-  status: AllPermits_permits_items_status;
-}
-
-export interface AllPermits_permits {
-  items: AllPermits_permits_items[];
-  cursor: string | null;
-  moreResults: boolean | null;
-}
-
-export interface AllPermits {
-  permits: AllPermits_permits;
-}
-
-export interface AllPermitsVariables {
-  status?: string | null;
-  orderBy?: PermitOrderBy | null;
-  cursor?: string | null;
-}
-
-/* tslint:disable */
-// This file was automatically generated and should not be edited.
-
-// ====================================================
-// GraphQL query operation: PermitDetails
-// ====================================================
-
-export interface PermitDetails_permitById_attachments {
-  id: string;
-  filename: string;
-}
-
-export interface PermitDetails_permitById {
-  id: string;
-  dateAndTime: any;
-  durationInMinutes: number;
-  location: string;
-  rejectionReason: string | null;
-  pdfUrl: string | null;
-  name: string;
-  email: string;
-  mobileNumber: string | null;
-  attachments: PermitDetails_permitById_attachments[] | null;
-}
-
-export interface PermitDetails {
-  permitById: PermitDetails_permitById | null;
-}
-
-export interface PermitDetailsVariables {
-  id: string;
-}
-
-/* tslint:disable */
-// This file was automatically generated and should not be edited.
-
-// ====================================================
-// GraphQL query operation: PermitsByMobileNumber
-// ====================================================
-
-export interface PermitsByMobileNumber_permits_items_status {
-  key: string;
-  description: string;
-}
-
-export interface PermitsByMobileNumber_permits_items {
-  id: string;
-  description: string;
-  dateAndTime: any;
-  name: string;
-  status: PermitsByMobileNumber_permits_items_status;
-}
-
-export interface PermitsByMobileNumber_permits {
-  items: PermitsByMobileNumber_permits_items[];
-}
-
-export interface PermitsByMobileNumber {
-  permits: PermitsByMobileNumber_permits;
-}
-
-export interface PermitsByMobileNumberVariables {
-  mobileNumber: string;
-}
-
-/* tslint:disable */
-// This file was automatically generated and should not be edited.
-
-// ====================================================
-// GraphQL query operation: PermitDetailsHeader
-// ====================================================
-
-export interface PermitDetailsHeader_permitById_status {
-  key: string;
-  description: string;
-}
-
-export interface PermitDetailsHeader_permitById {
-  id: string;
-  description: string;
-  dateAndTime: any;
-  name: string;
-  status: PermitDetailsHeader_permitById_status;
-}
-
-export interface PermitDetailsHeader {
-  permitById: PermitDetailsHeader_permitById | null;
-}
-
-export interface PermitDetailsHeaderVariables {
-  id: string;
-}
-
-/* tslint:disable */
-// This file was automatically generated and should not be edited.
-
-// ====================================================
-// GraphQL mutation operation: ApprovePermit
-// ====================================================
-
-export interface ApprovePermit_approvePermit_status {
-  key: string;
-  description: string;
-}
-
-export interface ApprovePermit_approvePermit {
-  id: string;
-  status: ApprovePermit_approvePermit_status;
-}
-
-export interface ApprovePermit {
-  approvePermit: ApprovePermit_approvePermit | null;
-}
-
-export interface ApprovePermitVariables {
-  id: string;
-}
-
-/* tslint:disable */
-// This file was automatically generated and should not be edited.
-
-// ====================================================
-// GraphQL mutation operation: RejectPermit
-// ====================================================
-
-export interface RejectPermit_rejectPermit_status {
-  key: string;
-  description: string;
-}
-
-export interface RejectPermit_rejectPermit {
-  id: string;
-  status: RejectPermit_rejectPermit_status;
-  rejectionReason: string | null;
-}
-
-export interface RejectPermit {
-  rejectPermit: RejectPermit_rejectPermit | null;
-}
-
-export interface RejectPermitVariables {
-  id: string;
-  reason: string;
-}
-
-/* tslint:disable */
-// This file was automatically generated and should not be edited.
-
-// ====================================================
 // GraphQL mutation operation: InviteUser
 // ====================================================
 
 export interface InviteUser {
-  inviteUser: boolean | null;
+  inviteUser: string | null;
 }
 
 export interface InviteUserVariables {
@@ -310,7 +91,7 @@ export interface ListUsers_users_avatar {
 
 export interface ListUsers_users {
   id: string;
-  name: string;
+  name: string | null;
   email: string;
   avatar: ListUsers_users_avatar;
   roles: string[];
@@ -334,7 +115,7 @@ export interface UserDetails_userById_credentials {
 
 export interface UserDetails_userById {
   id: string;
-  name: string;
+  name: string | null;
   roles: string[];
   credentials: UserDetails_userById_credentials | null;
 }
@@ -356,7 +137,7 @@ export interface UserDetailsVariables {
 
 export interface UpdateUser_updateUser {
   id: string;
-  name: string;
+  name: string | null;
   roles: string[];
 }
 
@@ -373,75 +154,9 @@ export interface UpdateUserVariables {
 /* tslint:disable */
 // This file was automatically generated and should not be edited.
 
-// ====================================================
-// GraphQL fragment: PermitCard
-// ====================================================
-
-export interface PermitCard_status {
-  key: string;
-  description: string;
-}
-
-export interface PermitCard {
-  id: string;
-  description: string;
-  dateAndTime: any;
-  name: string;
-  status: PermitCard_status;
-}
-
-/* tslint:disable */
-// This file was automatically generated and should not be edited.
-
-// ====================================================
-// GraphQL fragment: PermitStatus
-// ====================================================
-
-export interface PermitStatus {
-  key: string;
-  description: string;
-}
-
-/* tslint:disable */
-// This file was automatically generated and should not be edited.
-
 //==============================================================
 // START Enums and Input Objects
 //==============================================================
-
-export enum PermitSortField {
-  dateAndTime = 'dateAndTime',
-  name = 'name',
-}
-
-export enum SortDirection {
-  asc = 'asc',
-  desc = 'desc',
-}
-
-//
-export interface PermitApplication {
-  name: string;
-  email: string;
-  location: string;
-  dateAndTime: any;
-  description: string;
-  durationInMinutes: number;
-  mobileNumber?: string | null;
-  attachments?: PermitApplicationAttachment[] | null;
-}
-
-//
-export interface PermitApplicationAttachment {
-  id: string;
-  filename: string;
-}
-
-//
-export interface PermitOrderBy {
-  field: PermitSortField;
-  direction: SortDirection;
-}
 
 //==============================================================
 // END Enums and Input Objects
