05655e88b2f480a849fcc79150ad20aeae1828cc
Glenn Molnar <glenn.molnar@gmail.com>
2021-03-11 14:33:12 +1100

    fix broken user resolvers

--- generators/app/templates/server/src/users/users.resolver.ts
+++ generators/app/templates/server/src/users/users.resolver.ts
@@ -1,5 +1,5 @@
 import { Context, Roles, AllowAnonymous } from '@mondomob/gae-node-nestjs';
-import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Context as GqlContext, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { UserRepository } from './users.repository';
 import { UsersService } from './users.service';
 import { User } from './users.model';
@@ -20,22 +20,22 @@ export class UsersResolver {
   async users(_obj: {}, _args: {}, context: Context): Promise<User[]> {
     const [users] = await this.userRepository.query(context);
 
-    return users as any as User[];
+    return (users as any) as User[];
   }
 
   @Query(() => User)
-  async userById(_obj: void, @Args('id', { type: () => ID }) id: string, context: Context) {
+  async userById(@Args('id', { type: () => ID }) id: string, @GqlContext() context: Context) {
     return this.userRepository.get(context, id);
   }
 
   @Roles('admin')
   @Mutation(() => User)
-  async updateUser(_req: void,
-                   @Args('roles', { type: () => [String!] }) roles: string[],
-                   @Args('name') name: string,
-                   @Args('id', { type: () => ID }) id: string,
-                   context: Context) {
+  async updateUser(
+    @Args('roles', { type: () => [String!] }) roles: string[],
+    @Args('name') name: string,
+    @Args('id', { type: () => ID }) id: string,
+    @GqlContext() context: Context,
+  ) {
     return await this.userService.update(context, id, { name, roles });
   }
-
 }
